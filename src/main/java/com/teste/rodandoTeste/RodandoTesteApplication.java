7624374e8d8140c38ac0dbb53d50322abf3929ad
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-31 14:18:18 -0300

    subindo java na pipeline

---
+++ src/main/java/com/teste/rodandoTeste/RodandoTesteApplication.java
@@ -0,0 +1,13 @@
+package com.teste.rodandoTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodandoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodandoTesteApplication.class, args);
+	}
+
+}
